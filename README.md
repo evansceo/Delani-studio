3731bcffa3eea84b7423fd2ba9cbf17de6df3e94
evansceo <evans.onyiego@student.moringaschool.com>
2021-09-14 22:53:44 +0300

    gh-pages

---
+++ README.md
@@ -0,0 +1,46 @@
+# Delani-studio
+## Author
+Evans Oduor Onyiego
+## Description
+This project is a web application for Delani studio that shows details about the services it offers, has information about the studio, a portfolio section and a form that enables users to send their messages and concern to Delani studios.
+### Prerequisities
+You need to have git installed
+You can install it with the following command in your terminal.
+`$ sudo apt install git-all`
+### Setup
+This project can be accesed and downloaded to your local files, you can clone it in these simple steps:
+1. Open your terminal
+2. use this command to clone '$ git clone https://github.com/evansceo/Delani-studio.git
+3. This will clone the repository in your local folder
+### Behavior Driven Development
+And the following terms exist:
+|Section     | Terms                                   | 
+|:----------:|:---------------------------------------:|
+|About Us    |work, collaborators, development, process|
+|Services    |sign, contract, write, product, vision   |
+|What we do  |design, development, product management  |
+|Portfolio   |studio, security, pyraminds, jim         |
+|Contact us  |name, email, your message                |
+
+
+How contact section works:
+|Behaviour     | Input                    | Output       |
+|:------------:|:------------------------:|-------------:|
+|Type name     |Insert words              |name spelling |
+|Type email    |Insert email              |email address |
+|Type message  |write message             |message body  |
+|Submit        |click                     |message sent  |
+### Technologies used
+1. HTML
+2. CSS
+3. Git
+4. JavaScript
+5. BootStrap
+6. jQuery
+### Live site
+View site (https://evansceo.github.io/Delani-studio/)
+### Contacts
+|Phone: |+254712234913|
+|Email: |evansville09@gmail.com|
+### License
+This project is under the [MIT](license) license
---
+++ index.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+    $("#DESIGN-img").click(function () {
+        $("#DESIGN-img").fadeOut("slow").hide('1000')
+        $("#DESIGNS").fadeIn("slow")
+    });
+    $("#DESIGNS").click(function () {
+        $("#DESIGNS").toggle("slow").hide('1000')
+        $("#DESIGN-img").fadeIn("slow")
+    });
+});
+$(document).ready(function () {
+    $("#dev-img").click(function () {
+        $("#dev-img").fadeOut("slow").hide('1000')
+        $("#dev").fadeIn("slow")
+    });
+    $("#dev").click(function () {
+        $("#dev").toggle("slow").hide('1000')
+        $("#dev-img").fadeIn("slow")
+    });
+});
+$(document).ready(function () {
+    $("#product-img").click(function () {
+        $("#product-img").fadeOut("slow").hide('1000')
+        $("#product").fadeIn("slow")
+    });
+    $("#product").click(function () {
+        $("#product").toggle("slow").hide('1000')
+        $("#product-img").fadeIn("slow")
+    });
+});
+
+$(document).ready(function () {
+    $("#portfolio-1").mouseover(function () {
+        $("#done1").show();
+    }).mouseout(function () {
+        $("#done1").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-2").mouseover(function () {
+        $("#done2").show();
+    }).mouseout(function () {
+        $("#done2").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-3").mouseover(function () {
+        $("#done3").show();
+    }).mouseout(function () {
+        $("#done3").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-4").mouseover(function () {
+        $("#done4").show();
+    }).mouseout(function () {
+        $("#done4").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-5").mouseover(function () {
+        $("#done5").show();
+    }).mouseout(function () {
+        $("#done5").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-6").mouseover(function () {
+        $("#done6").show();
+    }).mouseout(function () {
+        $("#done6").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-7").mouseover(function () {
+        $("#done7").show();
+    }).mouseout(function () {
+        $("#done7").hide();
+    });
+});
+$(document).ready(function () {
+    $("#portfolio-8").mouseover(function () {
+        $("#done8").show();
+    }).mouseout(function () {
+        $("#done8").hide();
+    });
+});
+$(document).ready(function () {
+    $("form#form").submit(function (event) {
+        event.preventDefault()
+    });
+    $("#sub").click(function () {
+        var name = $("input#name").val()
+        var email = $("input#email").val()
+        var message = $("textarea#comment").val()
+        if ($("input#name").val() && $("input#email").val()) {
+            alert("Please enter a valid email.")
+        } else {
+            alert("Thank you for your feedback, our personnel will reach out to you.")
+        }
+    });
+});
